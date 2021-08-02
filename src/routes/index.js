5bc5d67b606e22a0012ad8037b69f1dddb4be553
Douglas Andrade <dba12@hotmail.com>
2021-08-02 20:04:58 -0300

    Iniciando a página de produtos

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,13 @@ const path = require('path')
 routes.get('/', (req, res) => {
     res.sendFile(path.resolve("src/views", "home.html"))
 })
+
 routes.get('/home', (req, res) => {
     res.redirect('/')
+})
+
+routes.get('/products', (req, res) => {
+res.sendFile(path.resolve("src/views","products.html"))
 })
 
 module.exports= routes
