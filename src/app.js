b24f10ecef5fec63299f7488a25bb214c10bb614
Wendel Cutrim <wendelcutrim@hotmail.com>
2021-08-01 22:31:06 -0300

    Realizado a criação do modelo Default no arquivo global.css e global.html

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 7000
 const routes = require('./routes/index')
 
+app.use(express.static("src/public"))
 app.use(express.json())
 app.use(routes)
-app.use(express.static("public"))
 app.use(express.urlencoded({ extended: false }))
 app.listen(port, () => console.log(`The server is runing on port:${port}`))
