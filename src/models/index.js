51d2ecd2eca003a562445a43777f3e013ffc2d14
Douglas Andrade <dba12@hotmail.com>
2021-10-28 02:14:45 -0300

    update

--- src/models/index.js
+++ src/models/index.js
@@ -5,11 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-<<<<<<< HEAD
 const config = require("../config/database")["development"];
-=======
-const config = require("../config/database");
->>>>>>> dev
 const db = {};
 
 let sequelize;
